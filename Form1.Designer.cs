da1914f9785654a65c8c1f3764831e6730a2082b
nicolaudev-hub <davinicolauti@gmail.com>
2026-04-07 21:59:47 -0300

    feat: cadastro de transações funcionando com validação e DataGrid

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,23 +42,23 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(366, 28);
+            this.label1.Location = new System.Drawing.Point(117, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Descrição";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(452, 83);
+            this.textBox1.Location = new System.Drawing.Point(120, 38);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "txtDescricao";
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(466, 171);
+            this.label2.Location = new System.Drawing.Point(117, 61);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 0;
@@ -66,16 +66,16 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(423, 258);
+            this.textBox2.Location = new System.Drawing.Point(120, 77);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 2;
-            this.textBox2.Text = "txtValor";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(353, 316);
+            this.label3.Location = new System.Drawing.Point(117, 98);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(28, 13);
             this.label3.TabIndex = 3;
@@ -89,7 +89,7 @@
             this.cmbTipo.Items.AddRange(new object[] {
             "Receita",
             "Despesa"});
-            this.cmbTipo.Location = new System.Drawing.Point(553, 355);
+            this.cmbTipo.Location = new System.Drawing.Point(120, 114);
             this.cmbTipo.Name = "cmbTipo";
             this.cmbTipo.Size = new System.Drawing.Size(121, 21);
             this.cmbTipo.TabIndex = 4;
@@ -97,20 +97,22 @@
             // dgvTransacoes
             // 
             this.dgvTransacoes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvTransacoes.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvTransacoes.Location = new System.Drawing.Point(0, 0);
+            this.dgvTransacoes.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvTransacoes.Location = new System.Drawing.Point(0, 206);
             this.dgvTransacoes.Name = "dgvTransacoes";
-            this.dgvTransacoes.Size = new System.Drawing.Size(800, 450);
+            this.dgvTransacoes.Size = new System.Drawing.Size(800, 244);
             this.dgvTransacoes.TabIndex = 5;
+            this.dgvTransacoes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvTransacoes_CellContentClick);
             // 
             // btnAdicionar
             // 
-            this.btnAdicionar.Location = new System.Drawing.Point(416, 140);
+            this.btnAdicionar.Location = new System.Drawing.Point(120, 141);
             this.btnAdicionar.Name = "btnAdicionar";
             this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
             this.btnAdicionar.TabIndex = 6;
             this.btnAdicionar.Text = "Adicionar";
             this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
             // 
             // Form1
             // 
@@ -127,6 +129,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
             ((System.ComponentModel.ISupportInitialize)(this.dgvTransacoes)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Drawing.Text;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -26,5 +27,67 @@ namespace gerenciador_financeiro_csharp
         {
             CarregarDados();
         }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(textBox1.Text))
+                {
+                    MessageBox.Show("Digite a descrição!");
+                    return;
+
+                }
+                if (!decimal.TryParse(textBox2.Text, out decimal valor))
+                {
+
+                    MessageBox.Show("Digite um valor válido!");
+                    return;
+                }
+                if (cmbTipo.SelectedItem == null)
+                {
+                    MessageBox.Show("Selecione um tipo!");
+                    return;
+                }
+
+                Transacao t = new Transacao
+                {
+                    Descricao = textBox1.Text,
+                        Valor = valor,
+                        Tipo = cmbTipo.Text,
+                        Data = DateTime.Now
+                };
+                repo.Adicionar(t);
+                CarregarDados();
+                MessageBox.Show("Salvo com sucessao!");
+            }
+                    catch(Exception ex)
+             {
+                    MessageBox.Show("Erro: " + ex.Message);
+             }
+        }
+        private void dgvTransacoes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            cmbTipo.Items.Add("Entrada");
+            cmbTipo.Items.Add("Saída");
+            cmbTipo.SelectedIndex = 0;
+            CarregarDados();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
-} 
+}
